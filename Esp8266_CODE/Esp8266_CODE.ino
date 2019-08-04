e02c3f35abdcbb7a8dfb3d31c55bdc24e62d9bbe
kashinath mali <kashinathmali2015@gmail.com>
2019-08-04 20:59:43 +0530

    Add files via upload

---
+++ Esp8266_CODE/Esp8266_CODE.ino
@@ -0,0 +1,59 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "kashi";  //your hotspot name.
+const char* password = "kashi123456"; //your hotspot password.
+WiFiServer server(80);
+
+void printWiFiStatus();
+
+void setup(void) {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+
+
+  // Start TCP server.
+  server.begin();
+}
+
+void loop(void) {
+  // Check if module is still connected to WiFi.
+  if (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+    }
+    // Print the new IP to Serial.
+    printWiFiStatus();
+  }
+
+  WiFiClient client = server.available();
+
+  if (client) {
+    while (client.connected()) {
+      if (client.available()) {
+        String command = client.readStringUntil('\r');
+        Serial.print(command);
+        if (command == "A") {
+          Serial.print("A");
+        }
+        else if (command == "B") {
+          Serial.print("B");
+        }
+        else if (command == "C") {
+          Serial.print("C");
+        }
+        else if (command == "D") {
+          Serial.print("D");
+        }
+      }
+    }
+    client.stop();
+  }
+}
+
+void printWiFiStatus() {
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
